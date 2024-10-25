67c8ea5c567cfee9890411d5225a1a4dcee2158f
omkar <omdb2004@gmail.com>
2024-10-25 23:40:20 +0530

    h1

--- frontend/src/views/timeclock/Timeclock.js
+++ frontend/src/views/timeclock/Timeclock.js
@@ -1,7 +1,12 @@
 import React from 'react'
 
 function Timeclock() {
-  return <div>Timeclock employee 200</div>
+  return (
+    <div>
+      Timeclock employee 200
+      <h1>hello</h1>
+    </div>
+  )
 }
 
 export default Timeclock
