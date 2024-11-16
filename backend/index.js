8c72e850a43314101659c81417caa7a3f064d3aa
omkaar <omdb2004@gmail.com>
2024-11-16 20:33:08 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -17,5 +17,5 @@ mongoose
 // Routes
 app.use("/api/employees", employeeRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
